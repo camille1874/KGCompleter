db256f67611252122a788eea62d91b35c5175c5f
camille1874 <15190971357@163.com>
2018-10-25 15:57:58 +0800

    add get_tuple

--- tools/text_processor.py
+++ tools/text_processor.py
@@ -3,11 +3,24 @@
 # @Author  : xuzh
 # @Project: KGCompleter
 
+import re
+import bs4
+
+def extract_tag(s):
+    res_str = r'<.*?>(.*?)</.*?>'
+    tmp_str = re.findall(res_str, s, re.S | re.M)
+    if tmp_str is not None and tmp_str != []:
+        return tmp_str[0]
+    else:
+        return s
+
 def clean_str(s):
-    if isinstance(s, list):
-        s = [clean_str(x) for x in s]
+    if isinstance(s, bs4.element.Tag):
+        s = extract_tag(s)
+    elif isinstance(s, list):
+        s = [clean_str(x.string) for x in s]
         s = "".join(s)
     if not isinstance(s, str):
         return ""
-    return s.replace("<a>", "").replace("</a>", "").replace("<", "").replace(">", "").replace("\n", "").strip()
+    return s.replace("\n", "").strip()
 
